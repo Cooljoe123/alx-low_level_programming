66144eda617e73ba6d7947320342129cba41637e
Cooljoe123 <Jobadjetey@gmail.com>
2022-12-12 11:43:27 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
-/**
- * main - This prints Programming is like building a multilingual puzzle
+/*
+ * main - This prints Programming is like building a multilingual puzzle using puts
  * Return:This prints 0
  */
 int main(void)
 {	
-	printf("Programming is like building a multilingual puzzle"\n);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
