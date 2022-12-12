13984e67b20a7a20a3b6aca27d2cf344b18aa2a9
Cooljoe123 <Jobadjetey@gmail.com>
2022-12-12 14:06:40 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - This prints Programming is like building a multilingual puzzle using puts
+ * main - Programming is like building a multilingual puzzle using puts
  *
- * Return:This prints 0
+ * Return:0
  */
 
 int main(void)
