7c1208e360a4d12a199f3ec4d09546790ce8e0a2
Cooljoe123 <Jobadjetey@gmail.com>
2022-12-18 07:59:11 -0800

    alphabet x 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,11 @@
 
 void print_alphabet_x10(void);
 {
-	int count = 0;
-	char letter;
+	int cnt = ;
+	char = i;
 
-	while (count++ <= 9)
-	for letter = ('a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (cnt = 1; cnt <= 10; cnt++)
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
 	_putchar('\n');
 }
